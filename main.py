106d12105e3e63df582bd97d9c639cea64b7e32d
finleynaughton1 <>
2021-04-23 12:31:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+
+def clear():
+  os.system('clear')
+
+my_secret = os.environ['password']
+red = "\033[1;31m"
+white = "\033[0;37m"
+
+password1 = int(input(white+"Enter your password (hint, it begins with 2 and ends in 8): "))
+if password1 < 27:
+  print("BEBAAWWBEREBAWW THE POLICE ARE HERE")
+elif password1 < 29:
+  clear()
+  print("Congrats police are coming to buckingham palace!\n")
+
+  password2 = int(input("Enter your password (hint it is one "+red+"number"+white+" long and is a multiple \nof 6): "))
+  if password2 < 5:
+    print("Blimey mate its hot outside")
+  elif password2 < 7:
+    clear()
+    print("You've done just fine")
+
+    botdetector2000 = str(input("If you are a computer then please type yes if not then type no: "))
+    if botdetector2000 == "Oui":
+
+      password3 = input("Final question, what is the super duper secret key that nobody knows execpt NASA")
+
+      if password3 == my_secret:
+        print("congratulations you have hacked NASA")
+
+        print(" ")
+        print(""+red+"Welcome to NASA")
+
+      else:
+        print("Ahh sorry mate, maybe next time! YOU WILL NEVER BE ACCEPTED TO NASA EVER AGAIN!")
+
+
+    
+    else:
+      print("NOPE JUST NOPE")
+
+  else:
+    print("Boo ya")
+
+else:
+  print("NOPE JUST NOPE")
+
